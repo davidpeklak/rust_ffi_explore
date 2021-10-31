566e7aee3b8b68d711b23faf846904a67b93463e
pekd <david.peklak@leonteq.com>
2021-10-31 21:47:18 +0100

    work with 2 names pipes

--- src/main.rs
+++ src/main.rs
@@ -4,18 +4,27 @@ use ffi_explore::file::File;
 use ffi_explore::poll::Poll;
 
 fn main() {
-    let path = CString::new("pipe1").unwrap();
+    let mut pipe1 = {
+        let path = CString::new("pipe1").unwrap();
+        File::new(path).unwrap()
+    };
 
-    let mut file = File::new(path).unwrap();
+    let mut pipe2 = {
+        let path = CString::new("pipe2").unwrap();
+        File::new(path).unwrap()
+    };
 
     let mut poll = Poll::new().unwrap();
 
-    poll.add(&file).unwrap();
+    poll.add(&pipe1).unwrap();
+    poll.add(&pipe2).unwrap();
 
     loop {
         println!("Waiting");
         poll.wait().unwrap();
-        let chunk = file.read().unwrap();
-        println!("{}", chunk.to_str().unwrap())
+        let chunk = pipe1.read().unwrap();
+        println!("pipe1: {}", chunk.to_str().unwrap());
+        let chunk = pipe2.read().unwrap();
+        println!("pipe2: {}", chunk.to_str().unwrap());
     }
 }
